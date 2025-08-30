8d9c4bd46da5b21d558ec9354d58d81da70e20ab
ShashanKSakilam <shashanksakilam@gmail.com>
2025-08-31 01:03:53 +0530

    shash

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
+    // ❌ don’t block build on ESLint errors
     ignoreDuringBuilds: true,
   },
   typescript: {
+    // ❌ don’t block build on TS errors
     ignoreBuildErrors: true,
   },
 };
